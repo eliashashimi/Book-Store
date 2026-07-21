354cc4d2c8452ec2537368d83aec3e5eeddb6d22
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-07-22 00:21:07 +0200

    changed style.css Bilder1-3 db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,7 +1,7 @@
 let books = [
     {
         "name": "Die Geheimnisse des Ozeans",
-        "image": "./assets/images/Bild_1.jpg",
+        "image": "./assets/images/Bild_1.png",
         "author": "Clara Meer",
         "likes": 1250,
         "liked": false,
@@ -33,7 +33,7 @@ let books = [
     },
     {
         "name": "Der vergessene Pfad",
-        "image": "./assets/images/Bild_2.jpg",
+        "image": "./assets/images/Bild_2.png",
         "author": "Maximilian Schwarz",
         "likes": 980,
         "liked": false,
@@ -44,7 +44,7 @@ let books = [
     },
     {
         "name": "Die Farben des Himmels",
-        "image": "./assets/images/Bild_3.jpg",
+        "image": "./assets/images/Bild_3.png",
         "author": "Laura Blau",
         "likes": 1520,
         "liked": true,
